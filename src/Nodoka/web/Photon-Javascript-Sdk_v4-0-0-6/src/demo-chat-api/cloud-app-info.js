6e17044ca1e3d1e5ebaaa7963493ade1e9369bbf
tang <maxtangli@qq.com>
2017-04-24 23:51:41 +0900

    remove photon sdk <- which causes JavaScript 56.1% > <

--- src/Nodoka/web/Photon-Javascript-Sdk_v4-0-0-6/src/demo-chat-api/cloud-app-info.js
+++
@@ -1,7 +0,0 @@
-﻿
-var AppInfo = {
-//	Wss: true,
-    AppId: "0dfe9a08-01d7-4b00-a678-290ff7ae63c5",
-    AppVersion: "1.0"
-}
-
--- src/Nodoka/web/Photon-Javascript-Sdk_v4-0-0-6/src/demo-particle/cloud-app-info.js
+++
@@ -1,8 +0,0 @@
-﻿
-var AppInfo = {
-//	Wss: true,	
-    AppId: "af8711f3-624f-4967-9964-3dfb3e2f6f3e",
-    AppVersion: "1.0",
-//    FbAppId: "you fb app id", 
-}
-
