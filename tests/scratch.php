1395a5c9cac6097ac91e3a696e0e91b935b88f34
tang <maxtangli@qq.com>
2017-10-22 22:53:41 +0900

    true solution: c 14 / 9x4.

--- tests/scratch.php
+++ tests/scratch.php
@@ -4,6 +4,7 @@ require_once __DIR__ . '/../bootstrap.php';
 
 // c 14 / 9x4
 
+// solution1: wrong! failed to consider 4 limit!
 $a = [];
 for ($i = 1; $i < 10; ++$i) {
     $s = '' . $i;
@@ -33,3 +34,20 @@ for ($i = 1; $i < 10; ++$i) {
 // 12
 // 13
 // 14 < 996 * 3^11 = 58812804; < 19440 x 3^7 = 42515280
+
+// solution2: iteration
+function f($kinds, $count) {
+    if ($kinds < 0 || $count < 0) return 0;
+    if ($kinds == 0 && $count == 0) return 1;
+    $sum = 0;
+    foreach (range(0, 4) as $takeCount) {
+        $sum += f($kinds - 1, $count - $takeCount);
+    }
+    return $sum;
+}
+
+echo f(1, 1) . "\n";
+echo f(1, 4) . "\n";
+echo f(9, 1) . "\n";
+echo f(2, 4) . "\n";
+echo f(9, 14) . "\n"; // 118800
